f7aa8bb6c82fb44e9965957437e47414b2d33552
Thais Rozas <thaisrozas@gmail.com>
2024-08-03 21:01:14 -0300

    feat: create next app and organize folder structure

---
+++ simple-meditation-website/app/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./globals.css";
+
+export default function Layout() {
+  return (
+    <html>
+      <body>      
+        <div>
+          <p className="texto">Olá</p>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ simple-meditation-website/app/page.tsx
@@ -0,0 +1,8 @@
+import Layout from "./layout";
+import "./globals.css";
+
+export default function Home() {
+  return (
+      <Layout></Layout>
+  );
+}
