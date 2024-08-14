f1a5a353dd1b6f22cc96018cc19045f014aa5d7d
ThaisRozas <thaisrozas@gmail.com>
2024-08-14 10:21:02 -0300

    refactor: bootstrap components for navbar

--- simple-meditation-website/components/Header.tsx
+++ simple-meditation-website/components/Header.tsx
@@ -1,55 +1,34 @@
 import React, {useState} from "react";
 //css only imported in global files
-import Link from "next/link";
+//import Link from "next/link";
 import Nav from 'react-bootstrap/Nav';
 import NavLink from "react-bootstrap/NavLink";
+import { Navbar, NavbarBrand, NavItem, NavbarCollapse, NavbarToggle, NavDropdown, DropdownItem, Dropdown } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 
 export default function Header(){
 
     // const [hover, setHover] = useState("hide");
-    /*<Nav className="nav-itens">
 
-            </Nav>*/
 
     return (
-        <header className="head">
-            <Link href="/" className="head-logo-name">
-                <img className="logo" src="simple-meditation-logo.png" alt="Simple meditation logo" />
-                <h4 className="heading4">Simple meditation</h4>
-            </Link>
-            <Nav className="nav-itens">
-            <div className="nav-itens-item">
-                    <NavLink className="heading5">About</NavLink>
-                    <img src="keyboard-arrow-down.svg" alt="" />
-                </div>
-                <div className="nav-itens-item">
-                    <h5 className="heading5">Meditation</h5>
-                    <img src="keyboard-arrow-down.svg" alt="" />
-                    <NavLink></NavLink>
-                </div>
-                <div className="nav-itens-item">
-                    <h5 className="heading5">Mindfullness</h5>
-                    <img src="keyboard-arrow-down.svg" alt="" />
-                    <NavLink></NavLink>
-                </div>
-                <div className="nav-itens-item">
-                    <h5 className="heading5">Plans</h5>
-                    <img src="keyboard-arrow-down.svg" alt="" />
-                    <NavLink></NavLink>
-                </div>
-                <div className="nav-itens-item">
-                    <h5 className="heading5">Sign in</h5>
-                    <img src="keyboard-arrow-down.svg" alt="" />
-                    <NavLink></NavLink>
-                </div>
-                <div className="nav-itens-item register">
-                    <h5 className="heading6">Register</h5>
-                    <NavLink></NavLink>
-                </div>
-
-            </Nav>
-
-        </header>
+        <Navbar>
+            <Container className="head">
+                <NavbarBrand className="head-logo-name" href="#">
+                    <img className="logo" src="simple-meditation-logo.png" alt="Simple meditation logo" />
+                    <h4 className="heading4">Simple meditation</h4>
+                </NavbarBrand>
+                
+                    <Nav className="nav-itens">
+                        <NavLink className="heading5">About</NavLink>
+                        <NavLink className="heading5">Community</NavLink>
+                        <NavLink className="heading5">Meditation and Mindfulness</NavLink>
+                        <NavLink className="heading5">Plans</NavLink>
+                        <NavLink className="heading5">Sign In</NavLink>
+                        <NavLink className="register heading6">Register</NavLink>
+                    </Nav>
+            </Container>
+        </Navbar>
     );
 }
